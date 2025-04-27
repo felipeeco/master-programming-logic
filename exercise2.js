66833eb526f8848f11e364603b0dfb1ca948d022
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2025-04-26 20:11:21 -0500

    second exercise

---
+++ exercise2.js
@@ -0,0 +1,10 @@
+// Dada una cadena de texto, comprobar si es un palindromo o no.
+//Given a string, check if it is a palindrome or not.
+
+function detectPalindrome(word) {
+  const reverseWord = word.split('').reverse().join('');
+  return (reverseWord === word)
+}
+
+const print = detectPalindrome('ana');
+console.log(print)
